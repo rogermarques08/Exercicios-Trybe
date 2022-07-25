acde63091d9f7a2ab02985c1e3b9a45a6fd40b0d
Roger Marques <marquesroger226@gmail.com>
2022-07-25 20:43:30 -0300

    Maior indice

--- 1-Fundamentos/4-Introdução à JavaScript e Lógica de Programação/JavaScript - Objetos e funções/Ex01/exercicio.js
+++ 1-Fundamentos/4-Introdução à JavaScript e Lógica de Programação/JavaScript - Objetos e funções/Ex01/exercicio.js
@@ -63,18 +63,18 @@
 // console.log(info.nota, 'e', info2.nota)
 
 
-let leitor = {
-    nome: 'Julia',
-    sobrenome: 'Pessoa',
-    idade: 21,
-    livrosFavoritos: [
-        {
-            titulo: 'O Pior Dia de Todos',
-            autor: 'Daniela Kopsch',
-            editora: 'Tordesilhas',
-        },
-    ],
-};
+// let leitor = {
+//     nome: 'Julia',
+//     sobrenome: 'Pessoa',
+//     idade: 21,
+//     livrosFavoritos: [
+//         {
+//             titulo: 'O Pior Dia de Todos',
+//             autor: 'Daniela Kopsch',
+//             editora: 'Tordesilhas',
+//         },
+//     ],
+// };
 
 //Exercicio 6
 
@@ -82,14 +82,48 @@ let leitor = {
 
 //Exercicio 7
 
-leitor.livrosFavoritos[1] = {
-    titulo: 'Harry Potter e o Prisioneiro de Azkaban',
-    autor: 'JK Rowling',
-    editora: 'Rocco',
-  }
-  
+// leitor.livrosFavoritos[1] = {
+//     titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+//     autor: 'JK Rowling',
+//     editora: 'Rocco',
+//   }
+
 //   console.log(leitor)
 
-  //Exercicio 8
+//Exercicio 8
 
-  console.log('Julia tem', leitor.livrosFavoritos.length, 'livros favoritos')
+//   console.log('Julia tem', leitor.livrosFavoritos.length, 'livros favoritos')
+
+//FUNÇÕES
+
+//Exercicio 1
+
+// function verificaPalindrome(word) {
+
+//     let reverse = '';
+//     for (let index = 0; index < word.length; index += 1) {
+//         reverse += word[word.length - 1 - index]
+//     }
+//     if (reverse == word) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+// console.log(verificaPalindrome('arara')); //true
+// console.log(verificaPalindrome('desenvolvimento')); //false
+
+function maiorIndice(numeros) {
+    let maior = numeros[0];
+    for (let index = 0; index < numeros.length; index += 1) {
+        if (numeros[index] > maior) {
+            maior = numeros[index]
+        }
+    }
+    maior = maior.length
+
+    return maior;
+}
+    
+console.log(maiorIndice([2, 3, 6, 7, 10, 1]))
