5d28ae8efa025558b6965807595fceaece2ddf69
Roger Marques <marquesroger226@gmail.com>
2022-07-28 18:50:42 -0300

    Exercicio 2 'eu acho'

--- 1-Fundamentos/ 5 - JavaScript: DOM, Eventos e Web Storage/JavaScript - Trabalhando com elementos/script.js
+++ 1-Fundamentos/ 5 - JavaScript: DOM, Eventos e Web Storage/JavaScript - Trabalhando com elementos/script.js
@@ -1,11 +1,19 @@
 // 🚀 Adicione a tag h1 com o texto Exercício 5.2 - JavaScript DOM como filho da tag body;
 
 function body() {
-    const corpo = document.getElementsByTagName('body')[0];
+    // const corpo = document.getElementsByTagName('body')[0];
     const filho = document.createElement('h1');
     filho.innerText = "Exercício 5.2 - JavaScript DOM como filho da tag body";
-    corpo.appendChild(filho)
-    console.log(corpo)
+    document.body.appendChild(filho)
 }
 body()
 
+//🚀 Adicione a tag main com a classe main-content como filho da tag body;
+
+function main() {
+    const son = document.createElement('main');
+    son.className = 'main-content'
+    document.body.appendChild(son);
+    console.log(body)
+}
+main()
