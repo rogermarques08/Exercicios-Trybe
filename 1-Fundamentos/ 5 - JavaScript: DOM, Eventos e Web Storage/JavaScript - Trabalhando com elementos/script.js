244d0d44b3315e4541a366a7a1ffc57fb9109ad4
Roger Marques <marquesroger226@gmail.com>
2022-07-28 21:19:08 -0300

    Exercicio 4

--- 1-Fundamentos/ 5 - JavaScript: DOM, Eventos e Web Storage/JavaScript - Trabalhando com elementos/script.js
+++ 1-Fundamentos/ 5 - JavaScript: DOM, Eventos e Web Storage/JavaScript - Trabalhando com elementos/script.js
@@ -125,5 +125,11 @@ function removeSection() {
 }
 removeSection()
 // 🚀 Centralize a section criado no passo 6 (aquele que possui a classe right-content). Dica: para centralizar, basta configurar o margin-right: auto da section;
+    function sectionCentralize() {
+        let section = document.getElementsByClassName('right-content')[0];
+        section.style.marginRight = 'auto'
+    }
+    sectionCentralize()
+
 // 🚀 Troque a cor de fundo do elemento pai da section criada no passo 3 (aquela que possui a classe center-content) para a cor verde;
 // 🚀 Remova os dois últimos elementos (nove e dez) da lista criada no passo 8.
