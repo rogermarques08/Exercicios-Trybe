7c345a02cf9988fdafd87b0b262dfcf510c3f4c7
Roger Marques <marquesroger226@gmail.com>
2022-08-12 18:03:28 -0300

    1.2

--- 1-Fundamentos/7 - Introdução à JavaScript ES6 e Testes Unitários/JavaScript ES6 - let, const, arrow functions e template literals/Ex01/script.js
+++ 1-Fundamentos/7 - Introdução à JavaScript ES6 e Testes Unitários/JavaScript ES6 - let, const, arrow functions e template literals/Ex01/script.js
@@ -3,18 +3,30 @@
 // Modifique as variáveis para que respeitem o escopo onde estão declaradas;
 // Modifique as concatenações para template literals.
 
-const testingScope = (escopo) => {
-  if (escopo === true) {
-    let ifScope = "Não devo ser utilizada fora do meu escopo (if)";
-    ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
-    console.log(ifScope);
-  } else {
-    var elseScope = "Não devo ser utilizada fora meu escopo (else)";
-    console.log(elseScope);
-  }
-  console.log(ifScope + " o que estou fazendo aqui ? :O"); // Se necessário esta linha pode ser removida.
-}
+// const testingScope = (escopo) => {
+//   if (escopo === true) {
+//     let ifScope = "Não devo ser utilizada fora do meu escopo (if)";
+//     ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+//     console.log(ifScope);
+//   } else {
+//     var elseScope = "Não devo ser utilizada fora meu escopo (else)";
+//     console.log(elseScope);
+//   }
+//   console.log(ifScope + " o que estou fazendo aqui ? :O"); // Se necessário esta linha pode ser removida.
+// }
 
-testingScope(true);
+// testingScope(true);
 
 
+// 🚀 Crie uma função que retorne um array em ordem crescente.
+// Crie uma função que faça com que o array oddsAndEvens fique em ordem crescente;
+// Utilize template literals para que a chamada console.log(<seu código>oddsAndEvens<seu código>); retorne a frase "Os números 2,3,4,7,10,13 se encontram ordenados de forma crescente!".
+// Bônus (opcional): tente fazer o mesmo exercício utilizando o método array.sort(). Spoiler: É possível realizar uma função que ordene qualquer array de números.
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+const sortOddsAndEvens = (array) => {
+    const numbers = array.sort((a , b) => a - b);
+    return numbers;
+};
+
+console.log(`Os números ${sortOddsAndEvens(oddsAndEvens)} se encontram em ordem crescernte`); // será necessário alterar essa linha 😉
