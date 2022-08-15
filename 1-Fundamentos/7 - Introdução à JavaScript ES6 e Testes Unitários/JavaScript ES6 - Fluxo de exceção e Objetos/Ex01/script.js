84bbd883afde9ae565e7f019e9cdbe7b36b0ec3d
Roger Marques <marquesroger226@gmail.com>
2022-08-15 19:19:34 -0300

    Finaliza o primeiro exercicio

---
+++ 1-Fundamentos/7 - Introdução à JavaScript ES6 e Testes Unitários/JavaScript ES6 - Fluxo de exceção e Objetos/Ex01/script.js
@@ -0,0 +1,26 @@
+
+  const verifyInput = (value1, value2) => {
+    if (!value1 || !value2) {
+      throw new Error('Preencha os campos para realizar a soma');
+    }
+  }
+
+function sum() {
+    try {
+    let value1 = document.getElementById("value1").value;
+    let value2 = document.getElementById("value2").value; 
+    verifyInput(value1, value2) 
+        const result = Number(value1) + Number(value2);
+        document.getElementById("result").innerHTML = `Resultado: ${result}`;
+    } catch (error) {
+        document.getElementById("result").innerText = error.message;
+    } finally {
+        document.getElementById('value1').value = ''; 
+        document.getElementById('value2').value = '';
+    }
+}
+window.onload = () => {
+    const button = document.getElementById("button");
+    button.addEventListener("click", sum);
+};
+
