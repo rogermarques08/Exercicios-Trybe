61857b317b4b989c9cdb0f1e2b428d0015eec251
Roger Marques <marquesroger226@gmail.com>
2022-08-24 15:26:01 -0300

    Finaliza o exercicio 1

---
+++ 1-Fundamentos/ 8 - Higher Order Functions do JavaScript ES6/JavaScript ES6 - Higher Order Functions - reduce/script.js
@@ -0,0 +1,15 @@
+// 1 - Dada uma matriz, transforme em um array.
+
+const arrays = [
+    ['1', '2', '3'],
+    [true],
+    [4, 5, 6],
+];
+
+// const array = () => arrays.reduce((acc, curr) => acc += curr)
+
+// console.log(array())
+
+const array = () => arrays.reduce((acc, curr) => acc.concat(curr, []))
+
+// console.log(array())
