3517375406035265c2369a16a268d3bf014d60fa
Roger Marques <marquesroger226@gmail.com>
2022-08-24 19:28:05 -0300

    Finaliza o exercicio 6

--- 1-Fundamentos/ 8 - Higher Order Functions do JavaScript ES6/JavaScript ES6 - Higher Order Functions - reduce/script.js
+++ 1-Fundamentos/ 8 - Higher Order Functions do JavaScript ES6/JavaScript ES6 - Higher Order Functions - reduce/script.js
@@ -101,3 +101,25 @@ const biggerName = () => books.reduce((acc, curr) => curr.name.length > acc.name
 
 // console.log(biggerName())
 
+// 🚀 5- Dada o array de nomes, retorne a quantidade de vezes em que aparecem a letra a maiúscula ou minúscula.
+
+const names = [
+  'Aanemarie', 'Adervandes', 'Akifusa',
+  'Abegildo', 'Adicellia', 'Aladonata',
+  'Abeladerco', 'Adieidy', 'Alarucha',
+];
+
+
+// 🚀 6- Para o próximo exercício você deve criar um novo array de objetos a partir das informações das constantes students e grades, onde cada objeto desse novo array terá o formato { name: nome do aluno, average: media das notas }. Para isso vamos assumir que a posição do index 0 de grades refere-se ao estudante na posição de index 0 de students. Para resolver este exercício, além de reduce, será necessário utilizar também a função map. Dica: Você pode acessar o index do array dentro de map, e você pode ver o objeto esperado na constante expected. 
+
+const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
+const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
+
+const newArray = () => students.map((element, index) => {
+  return {
+    name : element,
+    average : grades[index].reduce((acc, curr) => acc + curr, 0) / grades[index].length
+  }
+})
+
+console.log(newArray())
